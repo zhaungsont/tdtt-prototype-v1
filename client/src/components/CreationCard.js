e4659a2ca02b7e9f870559484a026872ad41d184
Michael Chuang <81564341+zhaungsont@users.noreply.github.com>
2022-03-23 15:38:10 +0800

    Add Property Behavior (unfinished)

--- client/src/components/CreationCard.js
+++ client/src/components/CreationCard.js
@@ -1,5 +1,6 @@
 import classes from "./CreationCard.module.css";
 import { useState, useRef, useEffect } from "react";
+import Backdrop from "./Backdrop";
 
 
 function CreationCard(){
@@ -19,34 +20,71 @@ function CreationCard(){
     //       }
     //     }, [isEditing]);
     // }
+    const [ESD, setESD] = useState(false);
 
+    function openESD(){
+        closeAllProperties()
+        setESD(!ESD);
+    }
+
+    const [EED, setEED] = useState(false);
+
+    function openEED(){
+        closeAllProperties()
+        setEED(!EED);
+    }
+
+    const [EST, setEST] = useState(false);
+
+    function openEST(){
+        closeAllProperties()
+        setEST(!EST);
+    }
+
+    const [EET, setEET] = useState(false);
+
+    function openEET(){
+        closeAllProperties()
+        setEET(!EET);
+    }
+
+
+    function closeAllProperties(){
+        setESD(false);
+        setEED(false);
+        setEST(false);
+        setEET(false);
+    }
     return(
         <div className={classes.card}>
             <form>
-                <input className={classes.title} name="title" autoFocus placeholder="Title"></input>
-                <textarea  rows="4" name="description" placeholder="Note"></textarea>
+                <input onClick={closeAllProperties} className={classes.title} name="title" autoFocus placeholder="Title"></input>
+                <textarea onClick={closeAllProperties} rows="4" name="description" placeholder="Note"></textarea>
                 
                 <div className={classes.properties}>
-                    <div>
+                    <img onClick={openESD} src={require("../dummy-data/icons/esd.png")}></img>
+                    {ESD && <div>
                         <label htmlFor="esd">Estimated Start Date: </label>
-                        <input type="date" id="esd" name="esd"
-                            value="2000-01-09">
-                        </input>
-                    </div>
-                    <div>
+                        <input type="date" id="esd" name="esd"></input>
+                    </div>}
+
+                    <img onClick={openEED} src={require("../dummy-data/icons/asd.png")}></img>
+                    {EED && <div>
                         <label htmlFor="eed">Estimated End Date: </label>
-                        <input type="date" id="eed" name="eed"
-                            value="2000-01-09">
-                        </input>
-                    </div>
-                    <div>
+                        <input type="date" id="eed" name="eed"></input>
+                    </div>}
+
+                    <img onClick={openEST} src={require("../dummy-data/icons/est.png")}></img>
+                    {EST && <div>
                         <label htmlFor="est">Estimated Start Time:</label>
                         <input type="time" id="est" name="est"></input>
-                    </div>
-                    <div>
+                    </div>}
+
+                    <img onClick={openEET} src={require("../dummy-data/icons/ast.png")}></img>
+                    {EET && <div>
                         <label htmlFor="eet">Estimated End Time:</label>
                         <input type="time" id="eet" name="eet"></input>
-                    </div>
+                    </div>}
                 </div>
                 <div className={classes.spacer}></div>
 
