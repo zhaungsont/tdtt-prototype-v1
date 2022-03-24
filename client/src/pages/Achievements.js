228a5ddc64c45d093658b21516320c51590c71ca
Michael Chuang <81564341+zhaungsont@users.noreply.github.com>
2022-03-25 03:18:54 +0800

    Add meme achievement

--- client/src/pages/Achievements.js
+++ client/src/pages/Achievements.js
@@ -1,5 +1,6 @@
 import Sidebar from "../components/ui/Sidebar";
 import MainContent from "../components/ui/MainContent";
+import classes from "./Achievements.module.css";
 
 function Achievements(){
     return(
@@ -7,7 +8,14 @@ function Achievements(){
             <Sidebar />
             <MainContent>
                 <h1>Achievements</h1>
-                <p>Achievements Unlocked -- You Opened TDTT!</p>
+                
+                {/* https://tenor.com/view/rainbow-meme-mlg-frog-pepe-gif-17213585 */}
+                <h2 className={classes.rainbowTextAnimated}>
+                <img src={require('../dummy-data/icons/rainbow-meme.gif')} width='60'></img>
+                 Achievements Unlocked -- You Opened TDTT! 
+                <img src={require('../dummy-data/icons/rainbow-meme.gif')} width='60'></img>
+                </h2>
+
             </MainContent>
         </div>
     );
