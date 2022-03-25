c81c01604397d7226132a0c987b02cd143d4faa1
Michael Chuang <81564341+zhaungsont@users.noreply.github.com>
2022-03-26 01:45:55 +0800

    Add Login Component (not functional)

---
+++ client/src/pages/Login.js
@@ -0,0 +1,23 @@
+import classes from "./Login.module.css";
+
+function Login(){
+    return(
+        <div className={classes.loginWrapper}>
+            <form>
+                <label>
+                    <p>Username</p>
+                    <input type="text" />
+                </label>
+                <label>
+                    <p>Password</p>
+                    <input type="password" />
+                </label>
+                <div>
+                    <button type="submit">Submit</button>
+                </div>
+            </form>
+        </div>
+    );
+}
+
+export default Login;
