eb9d6fd90757af1cfb065fe375a4fa3aa1c770c8
Michael Chuang <81564341+zhaungsont@users.noreply.github.com>
2022-03-26 02:17:09 +0800

    Update Login.js

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,16 +1,28 @@
 import classes from "./Login.module.css";
+import { useState, useRef } from "react";
 
 function Login(){
+    const accountInputRef = useRef();
+    const passwordInputRef = useRef();
+
+    function submitHandler(event){
+        event.preventDefalt();
+
+        const account = accountInputRef.current.value;
+        const password = passwordInputRef.current.value;
+        const loginData = {account: account, password: password}
+    }
+    
     return(
         <div className={classes.loginWrapper}>
-            <form>
+            <form onSubmit={submitHandler}>
                 <label>
-                    <p>Username</p>
-                    <input type="text" />
+                    <p>Email</p>
+                    <input type="text" ref={usernameInputRef} />
                 </label>
                 <label>
                     <p>Password</p>
-                    <input type="password" />
+                    <input type="password" ref={passwordInputRef} />
                 </label>
                 <div>
                     <button type="submit">Submit</button>
